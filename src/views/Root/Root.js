01d3391f74423c3b50f13f476ba311bbc9aed1bb
DawidKopicki <dawidkopicki@gmail.com>
2019-10-30 15:31:51 +0100

    Project restructure, creating Button component

---
+++ src/views/Root/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <div>
+    <h1>Hello World!</h1>
+    <Button>Click me!</Button>
+  </div>
+);
+
+export default Root;

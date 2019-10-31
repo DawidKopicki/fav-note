fd3e3dc750a72f39ea18d38479ed0f97363140cf
DawidKopicki <dawidkopicki@gmail.com>
2019-10-31 10:45:28 +0100

    Atomic design restructure + ThemeProvider testing

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   padding: 0;
-  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
+  background-color: ${({ theme }) => theme.primary};
   width: ${({ width }) => width || '220px'};
   height: 47px;
   border: none;
@@ -21,5 +21,5 @@ const Button = styled.button`
       font-size: 10px;
     `}
 `;
-
+// background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
 export default Button;

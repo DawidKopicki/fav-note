29920a14e196d2ac695b0a795664b826f29d8e9a
DawidKopicki <dawidkopicki@gmail.com>
2019-10-31 07:01:16 +0100

    Props transmission refactor

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -4,7 +4,7 @@ import Button from 'components/Button/Button';
 const Root = () => (
   <div>
     <h1>Hello World!</h1>
-    <Button>Close / Save</Button>
+    <Button width="240px">Close / Save</Button>
     <Button secondary>Remove</Button>
   </div>
 );

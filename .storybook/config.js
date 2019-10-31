1419c71a4718b4b29b51a595dcfe35c16c394459
DawidKopicki <dawidkopicki@gmail.com>
2019-10-31 12:09:35 +0100

    Adding stories to new components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,8 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'theme/mainTheme';
 
 configure(require.context('../src', true, /\.stories\.js$/), module);
+
+addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
